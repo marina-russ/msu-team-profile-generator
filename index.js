583543dd376cbce6c070929ab1484e2bfe57959f
Marina Russ <marina@cameystudios.com>
2021-04-03 12:30:18 -0400

    changed roles/ to lib/, added questions

--- index.js
+++ index.js
@@ -1,11 +1,82 @@
 // Program Requirements:
 const inquirer = require("inquirer");
 const fs = require("fs");
-const util = require("util");
-
-// Page Requirements
+const htmlRenderer = require('./lib/renderHTML');
+// Roles:
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const Html = require("./src/htmlTemp");
-const validator = require("email-validator");
+// Questions needed for each Role:
+const { defineEmployee, employeeQuestions, engineerQuestions, internQuestions, managerQuestions} = require('./lib/questions');
+// Array of all employees
+const employeeArray = [];
+
+// Start Inquirer to gather user data
+function init() {
+  inquirer
+    .prompt(employeeQuestions.concat(managerQuestions))
+    .then(({ name, id, email, officeNumber }) => {
+      console.log(name, id, email, officeNumber);
+      let manager = new Manager(name, id, email, officeNumber);
+      employeeArray.push(manager);
+      getEmployee();
+    })
+}
+
+// Defines Employee and determines job role
+function getEmployee() {
+  inquirer
+    .prompt(defineEmployee)
+    .then(({ jobRole }) => {
+      switch (jobRole) {
+        case 'Engineer':
+          getEngineer();
+          break;
+        case 'Intern':
+          getIntern();
+          break;
+        case 'Manager':
+          getManager();
+          break;
+        case 'Complete':
+          htmlRenderer(employeeArray);
+          break;
+      }
+    })
+}
+
+// Gets questions for Engineer, then adds data to array
+function getEngineer() {
+  inquirer
+    .prompt(employeeQuestions.concat(engineerQuestions))
+    .then(({ name, id, email, githubUsername }) => {
+      let engineer = new Engineer(name, id, email, githubUsername);
+      employeeArray.push(engineer);
+      getEmployee();
+    })
+}
+
+// Gets questions for Intern, then adds data to array
+function getIntern() {
+  inquirer
+    .prompt(employeeQuestions.concat(internQuestions))
+    .then(({ name, id, email, school }) => {
+      let intern = new Intern(name, id, email, school);
+      employeeArray.push(intern)
+      getEmployee();
+    })
+}
+
+// Gets questions for Manager, then adds data to array
+function getManager() {
+  inquirer
+    .prompt(employeeQuestions.concat(managerQuestions))
+    .then(({ name, id, email, office }) => {
+      let manager = new Manager(name, id, email, office);
+      employeeArray.push(manager)
+      getEmployee();
+    })
+}
+
+// Runs methods
+init();
