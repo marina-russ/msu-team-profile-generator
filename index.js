c6224b3f6f546fa72651212950553aa1cc753e0a
Marina Russ <marina@cameystudios.com>
2021-04-04 17:30:33 -0400

    adds css styling through bootstrap

--- index.js
+++ index.js
@@ -1,82 +1,130 @@
 // Program Requirements:
 const inquirer = require("inquirer");
 const fs = require("fs");
-const htmlRenderer = require('./lib/renderHTML');
+const util = require("util");
+const html = require('./lib/renderHTML');
 // Roles:
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-// Questions needed for each Role:
-const { defineEmployee, employeeQuestions, engineerQuestions, internQuestions, managerQuestions} = require('./lib/questions');
+
+// Set up Async functions
+const writeFileAsync = util.promisify(fs.writeFile);
+const appendFileAsync = util.promisify(fs.appendFile);
 // Array of all employees
-const employeeArray = [];
+let teamArray = [];
+let teamString = ``;
 
-// Start Inquirer to gather user data
-function init() {
-  inquirer
-    .prompt(employeeQuestions.concat(managerQuestions))
-    .then(({ name, id, email, officeNumber }) => {
-      console.log(name, id, email, officeNumber);
-      let manager = new Manager(name, id, email, officeNumber);
-      employeeArray.push(manager);
-      getEmployee();
-    })
+
+// Main function to run application
+async function main() {
+  try {
+    await prompt()
+
+    for (let i = 0; i < teamArray.length; i++) {
+      teamString = teamString + html.generateCard(teamArray[i]);
+    }
+
+    let finalHtml = html.generateHTML(teamString)
+    console.log("Generating index.html file....");
+    console.log("-------------------------------");
+
+    writeFileAsync("./output/index.html", finalHtml);
+    console.log("index.html file created successfully");
+    console.log("-------------------------------");
+
+  } catch (err) {
+    return console.log(err);
+  }
 }
 
-// Defines Employee and determines job role
-function getEmployee() {
-  inquirer
-    .prompt(defineEmployee)
-    .then(({ jobRole }) => {
-      switch (jobRole) {
-        case 'Engineer':
-          getEngineer();
-          break;
-        case 'Intern':
-          getIntern();
-          break;
-        case 'Manager':
-          getManager();
-          break;
-        case 'Complete':
-          htmlRenderer(employeeArray);
-          break;
+// Inquirer prompts to gather user generated data
+async function prompt() {
+  let responseDone = "";
+
+  do {
+    try {
+      console.log("-------------------------------");
+      let response = await inquirer.prompt([
+        {
+          type: "input",
+          name: "name",
+          message: "Employee Name: ",
+        },
+        {
+          type: "input",
+          name: "id",
+          message: "Employee ID number: ",
+        },
+        {
+          type: "input",
+          name: "email",
+          message: "Employee email address: ",
+        },
+        {
+          type: "list",
+          name: "role",
+          message: "What what is the employee's role: ",
+          choices: [
+            "Engineer",
+            "Intern",
+            "Manager"
+          ]
+        }
+      ]);
+
+      let response2 = ""
+
+      if (response.role === "Engineer") {
+        response2 = await inquirer.prompt([{
+          type: "input",
+          name: "x",
+          message: "Engineer's github username: ",
+        },]);
+
+        // Add to team Array
+        const engineer = new Engineer(response.name, response.id, response.email, response2.x);
+        teamArray.push(engineer);
+
+      } else if (response.role === "Intern") {
+        response2 = await inquirer.prompt([{
+          type: "input",
+          name: "x",
+          message: "Intern's school name: ",
+        },]);
+
+        // Add to team Array
+        const intern = new Intern(response.name, response.id, response.email, response2.x);
+        teamArray.push(intern);
+
+      } else if (response.role === "Manager") {
+        response2 = await inquirer.prompt([{
+          type: "input",
+          name: "x",
+          message: "Manager's office number: ",
+        },]);
+
+        // Add to team Array
+        const manager = new Manager(response.name, response.id, response.email, response2.x);
+        teamArray.push(manager);
       }
-    })
+
+    } catch (err) {
+      return console.log(err);
+    }
+
+    responseDone = await inquirer.prompt([{
+      type: "list",
+      name: "finish",
+      message: "Do you have another employee to enter? ",
+      choices: [
+        "Yes",
+        "No"
+      ]
+    },]);
+
+  } while (responseDone.finish === "Yes");
 }
 
-// Gets questions for Engineer, then adds data to array
-function getEngineer() {
-  inquirer
-    .prompt(employeeQuestions.concat(engineerQuestions))
-    .then(({ name, id, email, githubUsername }) => {
-      let engineer = new Engineer(name, id, email, githubUsername);
-      employeeArray.push(engineer);
-      getEmployee();
-    })
-}
-
-// Gets questions for Intern, then adds data to array
-function getIntern() {
-  inquirer
-    .prompt(employeeQuestions.concat(internQuestions))
-    .then(({ name, id, email, school }) => {
-      let intern = new Intern(name, id, email, school);
-      employeeArray.push(intern)
-      getEmployee();
-    })
-}
-
-// Gets questions for Manager, then adds data to array
-function getManager() {
-  inquirer
-    .prompt(employeeQuestions.concat(managerQuestions))
-    .then(({ name, id, email, office }) => {
-      let manager = new Manager(name, id, email, office);
-      employeeArray.push(manager)
-      getEmployee();
-    })
-}
-
-// Runs methods
-init();
+// Run Application
+main();
